d7a3b51fe81d7ae1365315f1fd7db8fd4bb1d9d9
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-15 23:49:12 +0100

    feature tested behaviour of update quality for Brie already on 50 as quality

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -6,12 +6,17 @@ describe("Gilded Rose", function() {
   let shopBrieBestQuality;
   beforeEach(function(){
     shopBrieNormal = new Shop([new Item("Aged Brie", 10, 10)]);
+    shopBrieBestQuality = new Shop([new Item("Aged Brie", 10, 50)])
   });
 
   describe("updating Brie quality", function(){
     it('Increases by 1 the value of quality and decreases by one the sell in in normal conditions', function(){
       shopBrieNormal.updateQuality()
       expect(shopBrieNormal.items[0]).toEqual({"name": "Aged Brie", "quality": 11, "sellIn": 9})
+    });
+    it('Does not increase the quality if quality is already 50', function(){
+      shopBrieBestQuality.updateQuality()
+      expect(shopBrieBestQuality.items[0]).toEqual({"name": "Aged Brie", "quality": 50, "sellIn": 9})
     });
   });
 });
