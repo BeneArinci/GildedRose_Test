55997eecbb7a8b582bcda6c52186fcfd1f1d67fa
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-15 23:34:02 +0100

    test for Brie, normal conditions. Passed

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,17 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+  let shopBrieNormal;
+  let shopBrieExpired;
+  let shopBrieBestQuality;
+  beforeEach(function(){
+    shopBrieNormal = new Shop([new Item("Aged Brie", 10, 10)]);
+  });
+
+  describe("updating Brie quality", function(){
+    it('Increases by 1 the value of quality and decreases by one the sell in in normal conditions', function(){
+      shopBrieNormal.updateQuality()
+      expect(shopBrieNormal.items[0]).toEqual({"name": "Aged Brie", "quality": 11, "sellIn": 9})
+    });
   });
 });
