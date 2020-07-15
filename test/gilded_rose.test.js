7d6cf33b0afad78b3d62a6e968361dac048d9cad
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-15 23:53:42 +0100

    refactoring: changed name Aged Brie to Brie

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -5,18 +5,19 @@ describe("Gilded Rose", function() {
   let shopBrieExpired;
   let shopBrieBestQuality;
   beforeEach(function(){
-    shopBrieNormal = new Shop([new Item("Aged Brie", 10, 10)]);
-    shopBrieBestQuality = new Shop([new Item("Aged Brie", 10, 50)])
+    shopBrieNormal = new Shop([new Item("Brie", 10, 10)]);
+    shopBrieBestQuality = new Shop([new Item("Brie", -5, 50)])
   });
 
   describe("updating Brie quality", function(){
     it('Increases by 1 the value of quality and decreases by one the sell in in normal conditions', function(){
       shopBrieNormal.updateQuality()
-      expect(shopBrieNormal.items[0]).toEqual({"name": "Aged Brie", "quality": 11, "sellIn": 9})
+      expect(shopBrieNormal.items[0]).toEqual({"name": "Brie", "quality": 11, "sellIn": 9})
     });
     it('Does not increase the quality if quality is already 50', function(){
       shopBrieBestQuality.updateQuality()
-      expect(shopBrieBestQuality.items[0]).toEqual({"name": "Aged Brie", "quality": 50, "sellIn": 9})
+      expect(shopBrieBestQuality.items[0]).toEqual({"name": "Brie", "quality": 50, "sellIn": -6})
     });
   });
+
 });
